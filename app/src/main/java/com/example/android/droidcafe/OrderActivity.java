7043656f58e916d46b818071555e828d439af449
Tze Yang <limty-wm19@student.tarc.edu.my>
2020-07-09 10:26:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/droidcafe/OrderActivity.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.droidcafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This activity shows the order chosen.  The order is sent as data
+ * with the intent to launch this activity.
+ */
+public class OrderActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        // Get the intent and its data.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+//        EditText editText = findViewById(R.id.phone_text);
+//        if (editText != null)
+  //          editText.setOnEditorActionListener
+ //                   (new TextView.OnEditorActionListener() {
+//
+  //                  });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_text));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_text));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up_text));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void onCheckBoxClicked (View view) {
+        // Is the button now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.chocolateSyrup:
+                if (checked)
+                    displayToast("Toppings: Chocolate Syrup");
+                break;
+            case R.id.sprinkles:
+                if (checked)
+                    displayToast("Toppings: Sprinkles");
+                break;
+            case R.id.crushedNuts:
+                if (checked)
+                    displayToast("Toppings: Crushed Nuts");
+                break;
+            case R.id.cherries:
+                if (checked)
+                    displayToast("Toppings: Cherries");
+                break;
+            case R.id.orioCookies:
+                if (checked)
+                    displayToast("Toppings: Orio Cookies Crumbles");
+                break;
+
+            default:break;
+        }
+    }
+}
+
+
